1ed1358f82b6bc6efaeeda078253386f01bf08b2
Max Goren <65133477+maxgoren@users.noreply.github.com>
2021-10-07 19:17:38 -0400

    Add files via upload

---
+++ secret_project/camelCase.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+# Only true gods use Perl
+#-Donald Knuth
+print "Hello, you crazy philosophers!\n";
---
+++ secret_project/java_code.java
@@ -0,0 +1,6 @@
+// inner dialogue, (C) Robert Sedgewick MIT License
+public class java_code {
+    public void main(String[] args) {
+        System.out.println("Surely, the rest will come around to java");
+    }
+}
